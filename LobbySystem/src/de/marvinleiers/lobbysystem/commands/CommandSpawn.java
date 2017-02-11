ff4c58eadc497e570cdb8d8537a52a3c1be96f1f
Marvin2k0 <Marvin@LAPTOP-THOM4JIA>
2017-02-12 00:03:19 +0100

    Modified CommandSpawn.java

--- LobbySystem/src/de/marvinleiers/lobbysystem/commands/CommandSpawn.java
+++ LobbySystem/src/de/marvinleiers/lobbysystem/commands/CommandSpawn.java
@@ -22,16 +22,7 @@ public class CommandSpawn implements CommandExecutor
         
         if (cmd.getName().equalsIgnoreCase("setspawn"))
         {
-          final Location loc = p.getLocation();
-
-          this.config.set("spawn.world", loc.getWorld().getName());
-          this.config.set("spawn.x", loc.getX());
-          this.config.set("spawn.y", loc.getY());
-          this.config.set("spawn.z", loc.getZ());
-          this.config.set("spawn.yaw", loc.getYaw());
-          this.config.set("spawn.pitch", loc.getPitch());
-
-          Main.getInstance().saveConfig();
+          Main.getInstance().getSpawnManager().setSpawn(p.getLocation());
 
           p.sendMessage("§aSpawn wurde gesetzt!");
 
@@ -41,9 +32,7 @@ public class CommandSpawn implements CommandExecutor
         {
           if (this.config.isSet("spawn"))
           {
-            Location spawn = new Location(Bukkit.getWorld(this.config.getString("spawn.world")), this.config.getDouble("spawn.x"), this.config.getDouble("spawn.y"), this.config.getDouble("spawn.z"));
-            spawn.setYaw((float) this.config.getDouble("spawn.yaw"));
-            spawn.setPitch((float) this.config.getDouble("spawn.pitch"));
+            Location spawn = Main.getInstance().getSpawnManager().getSpawn();
 
             p.teleport(spawn);
 
